8775e55d474b13902a05f250105014a7cafa7461
hongzhonglu <luho@chalmers.se>
2021-01-23 17:16:09 +0100

    fix: refine the readme

--- ComplementaryScripts/updateProductList.py
+++ ComplementaryScripts/updateProductList.py
@@ -13,14 +13,15 @@ part2 = lines[index0:]
 
 
 newline1 = '* Product list:\n'
-product_template ='| Name | Formula | KEGG ID | CHEBI ID | Group | class |\n'
-newline2 = '|:-------:|:--------------:|:---------:|:----------:|:-----:|:-----:|\n'
+product_template ='| Name | Formula | KEGG ID | CHEBI ID | Group | class | Gene target |\n'
+newline2 = '|:-------:|:--------------:|:---------:|:----------:|:-----:|:-----:|:-----:|\n'
 products_info = []
 for i, x in chemical_info.iterrows():
     print(i, x)
     newline0 = product_template.replace("Name", x["Name"]).replace("Formula", str(x["Formula"])).replace("KEGG ID", str(x["KEGG ID"])).\
         replace("CHEBI ID", str(x["CHEBI ID"])).replace("Group", x["Group"]).replace("class", x["class"])
-
+    # query the gene target prediction result
+    newline0 = newline0.replace("Gene target", "Link")
     products_info.append(newline0)
 
 # combine the product information as a readme file
