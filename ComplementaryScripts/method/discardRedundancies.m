a9853ebde6776ca123c05692ae9cefca5eaf17d7
Ivan Domenzain <ivand@chalmers.se>
2021-01-28 23:52:33 +0100

    fix: focus on deletion targets for removal of redundancies

--- ComplementaryScripts/method/discardRedundancies.m
+++ ComplementaryScripts/method/discardRedundancies.m
@@ -1,11 +1,13 @@
 function candidates = discardRedundancies(model,candidates)
 tempModel = model;
 threshold = 1E-12;
+original = candidates;
+candidates = candidates(~strcmpi(candidates.actions,'OE'),:);
 %Iterate through each priority level
+removed = [];
 for priority = [1 2 3]
-    disp(['*****Priority level: ' num2str(priority)])
     %Isolate deletion targets for the ith priority level
-    targets  = candidates(candidates.priority==priority & ~strcmpi(candidates.actions,'OE'),:);
+    targets  = candidates(candidates.priority==priority);
     if ~isempty(targets)
         %Rank targets by mean(pYield FC, pRate FC)
         %targets.FC = mean([targets.foldChange_yield targets.foldChange_pRate],2);
@@ -14,11 +16,11 @@ for priority = [1 2 3]
         i=1;
         while i<=height(targets)
             protein = targets.enzymes{i};
-            name = targets.shortNames(i);
+            name1 = targets.shortNames{i};
             %Check if gene still remains in candidates list
             [presence,iB] = ismember(protein,candidates.enzymes);
             if presence
-                disp(['Checking protein: ' name{1}])
+                %disp(['Checking protein: ' name{1}])
                 %delete gene
                 tempModel = removeGenes(model,candidates.genes(iB));
                 remaining = candidates(:,[1 2 3]);
@@ -27,7 +29,7 @@ for priority = [1 2 3]
                 %check if the remaining genes can still carry flux
                 for j = 1:height(remaining)
                     enzyme = remaining.enzymes{j};
-                    name = remaining.shortNames{j};
+                    name2 = remaining.shortNames{j};
                     enzRxn = ['draw_prot_' enzyme];
                     idx = find(strcmpi(tempModel.rxnNames,enzRxn));
                     if ~isempty(idx) & ismember(enzyme,candidates.enzymes)
@@ -37,9 +39,9 @@ for priority = [1 2 3]
                         if ~flux
                             position = find(strcmpi(candidates.enzymes,enzyme));
                             if ~isempty(position) & ~strcmpi(protein,enzyme)
-                                disp([' discarding ' name])
+                                disp([' Discarding ' name2 ' due to redundancy with ' name2])
                                 candidates(position,:) = [];
-                                %remaining(j,:) = [];
+                                removed = [removed;enzyme];
                             end
                         end
                     end
@@ -47,8 +49,9 @@ for priority = [1 2 3]
             end
             i = i+1;
         end
-        
-        
     end
 end
+[~,iB] = ismember(removed,original.enzymes);
+candidates = original;
+candidates(iB,:) = [];
 end
