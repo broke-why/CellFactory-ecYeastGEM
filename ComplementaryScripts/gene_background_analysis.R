c5133e0b392f1856b2644ee0444ad64cd42778fa
Ivan Domenzain <ivand@chalmers.se>
2021-02-19 00:11:55 +0100

    fix: merge KO and KD targets for mapping to experimental targets

--- ComplementaryScripts/gene_background_analysis.R
+++ ComplementaryScripts/gene_background_analysis.R
@@ -1,9 +1,9 @@
 # This script is used to compare the predicted genes from ecFSEOF and the genes used in the reference strains.
-
 library(ggplot2)
 library(tidyverse)
-library(hongR)
 library(readxl)
+library(pheatmap)
+library(viridis)
 
 # Setting the working directory to the directory which contains this script
 if (exists("RStudio.Version")){
@@ -27,7 +27,6 @@ mech_validated <- read.table("../results/targetsMatrix_mech_validated.txt", sep=
 # compatible
 compatible_gene <- read.table("../results/targetsMatrix_compatible.txt", sep="\t", header = TRUE, stringsAsFactors = FALSE)
 
-
 find_common_gene <- function(s1, s2) {
   common <- vector()
   # s1 <- background_OE
@@ -36,23 +35,28 @@ find_common_gene <- function(s1, s2) {
   s10 <- str_trim(s10, side = "both")
   s2 <- str_trim(s2, side = "both")
   common <- intersect(s10, s2)
+  perc   <- length(common)/length(s10)
   if (length(common) >=1) {
-    return(paste0(common,collapse = ","))
+    return(list(paste0(common,collapse = ","),perc))
   } else {
-    return(NA)
+    return(list(NA,perc))
   }
 }
 
 # comparison
 # the comparsion with start from ecFSEOF, mech_validated and compatible
 gene_background0$common_OE <- NA
-gene_background0$common_KD <- NA
-gene_background0$common_KO <- NA
+#gene_background0$common_KD <- NA
+gene_background0$common_KO_KD <- NA
+
+gene_background0$ovlp_OE <- 0
+#gene_background0$ovlp_KD <- 0
+gene_background0$ovlp_KO_KD <- 0
 
 met_name_need_check <- vector()
 for (i in 1:nrow(gene_background0)) {
   print(i)
-  product0 <- gene_background0$results_folder[i]
+  product0  <- gene_background0$results_folder[i]
   product00 <- str_replace(product0, "_targets", "")
   product00 <- product00 %>%
     str_replace_all(., "-", "") %>%
@@ -60,35 +64,34 @@ for (i in 1:nrow(gene_background0)) {
   product00 <- paste(product00, "_del_", sep = "")
   # as the name of products need to be unified
   product00 <- str_replace(product00, "^[:digit:]", "") # remove the number at the start of string
-
   background_OE <- gene_background0$overexpression[i]
-  background_KD <- gene_background0$downregulation[i]
-  background_KO <- gene_background0$deletion[i]
+  #background_KD <- paste(gene_background0$downregulation[i],gene_background0$deletion[i])
+  background_KO <- paste(gene_background0$downregulation[i],gene_background0$deletion[i],sep=',')
 
-  all_col <- colnames(ecFSEOF)
+  all_col      <- colnames(ecFSEOF)
+  common_OE    <- NA
+  common_KO_KD <- NA
   if (product00 %in% all_col) {
     # find the predicted genes based on the product name
     col_select <- c(c("genes", "shortNames", "enzymes", "subSystems"), product00)
     predict_tagets <- ecFSEOF[, col_select]
     predict_OE <- predict_tagets$genes[predict_tagets[[product00]] == 3]
-    predict_KD <- predict_tagets$genes[predict_tagets[[product00]] == 2]
-    predict_KO <- predict_tagets$genes[predict_tagets[[product00]] == 1]
-
-    common_OE <- find_common_gene(background_OE, predict_OE)
-    common_KD <- find_common_gene(background_KD, predict_KD)
-    common_KO <- find_common_gene(background_KO, predict_KO)
-
-    gene_background0$common_OE[i] <- common_OE
-    gene_background0$common_KD[i] <- common_KD
-    gene_background0$common_KO[i] <- common_KO
+    predict_KO <- predict_tagets$genes[predict_tagets[[product00]] == 1 | predict_tagets[[product00]] == 2]
+    #find intersect between predictions and data
+    #OEs
+    temp       <- find_common_gene(background_OE, predict_OE)
+    common_OE  <- temp[[1]]
+    gene_background0$ovlp_OE[i] <- temp[[2]]
+    #KOs and KDs
+    temp        <- find_common_gene(background_KO, predict_KO)
+    common_KO_KD <- temp[[1]]
+    gene_background0$ovlp_KO_KD[i] <- temp[[2]]
+    
   } else {
-    gene_background0$common_OE[i] <- NA
-    gene_background0$common_KD[i] <- NA
-    gene_background0$common_KO[i] <- NA
     met_name_need_check <- c(met_name_need_check, product00)
   }
+  gene_background0$common_OE[i]    <- common_OE
+  gene_background0$common_KO_KD[i] <- common_KO_KD
 }
-
 # save the analysis result
 write.table(gene_background0, "../results/Compare_predicted_targets_with_gene_background.txt", sep = "\t", row.names = FALSE)
-
