d8f87b3855d5752fe82db5134b98ec3812e4ef45
Ivan Domenzain <ivand@chalmers.se>
2022-10-03 18:56:39 +0200

    refactor: separate plot all chemicals function

--- code/analyse_results.R
+++ code/analyse_results.R
@@ -5,13 +5,13 @@ library(reshape)
 library(RColorBrewer)
 library(pheatmap)
 library(Rtsne)
+library(tibble)
 library(dplyr)
 library(tidyr)
 library(cluster)
 library(ggfortify)
 library(plotly)
 library(htmlwidgets)
-#library(qgraph)
 library(reshape2)
 
 
@@ -21,8 +21,8 @@ if (exists("RStudio.Version")){
 } else {
   setwd(getSrcDirectory()[1])
 }
-families <- c('amino acid','alkaloid','organic acid','protein','alcohol','terpene','fatty acids and lipids','flavonoid','aromatic','bioamine')
-codes    <- c('_AA','_alk','_oAc','_pro','_alc','_ter','_FA','_fla','_aro','_bioAm')
+families <- c('amino acid','alkaloid','organic acid','protein','alcohol','terpene','fatty acids and lipids','flavonoid','aromatic','bioamine','stilbenoids')
+codes    <- c('_AA','_alk','_oAc','_pro','_alc','_ter','_FAL','_fla','_aro','_bio','_stb')
 #Load targets summary
 filename        <- paste('../results/production_targets/targets_summary.txt',sep='')
 targets_summary <- read.csv(filename,sep='\t',stringsAsFactors = FALSE)
@@ -55,48 +55,45 @@ blank_theme <- theme_minimal()+
     legend.title = element_text(size=24,face="bold"),
     legend.text = element_text(size=18)
   )
-  
-p <- ggplot(df, aes(x='',y=perc,fill=classes)) +
-     geom_bar(colour = 'black',width = 1, stat = 'identity')
-     pie <- p + coord_polar("y", start=0,direction=1)
-     pie <- pie + blank_theme 
-     pie <- pie +scale_fill_manual(values = getPalette(colourCount))#scale_fill_viridis(discrete = T, option = "E")
-     pie <- pie + labs(fill = 'Chemical families')
-     png('../results/plots/chemicalFamilies.png',width = 800, height = 800)
-     plot(pie)
-     dev.off()
-
-
 #Analyse targets matrix
-allTargetsMat <- read.csv('../results/production_targets/targetsMatrix_compatible.txt',sep='\t',stringsAsFactors = TRUE)
-allTargetsMat <- read.csv('../results/production_targets/targetsMatrix_mech_validated.txt',sep='\t',stringsAsFactors = TRUE)
+allTargetsMat <- read.csv('../results/production_targets/targetsMatrix_L3_discrete.txt',sep='\t',stringsAsFactors = TRUE)
+#allTargetsMat <- read.csv('../results/production_targets/targetsMatrix_mech_validated.txt',sep='\t',stringsAsFactors = TRUE)
 targetsMat    <- allTargetsMat
 targetsMat <- targetsMat[rowSums(targetsMat[,5:ncol(targetsMat)])>0,]
+cases <- ncol(targetsMat) - 4
+idxs  <- which(rowSums(targetsMat[,5:ncol(targetsMat)])!=cases)
+reduced_matrix <- targetsMat[idxs,]
 #substitute values in matrix
-targetsMat[targetsMat == 2] <- 20
-targetsMat[targetsMat == 1] <- 10
-targetsMat[targetsMat == 3] <- 30
-targetsMat[targetsMat == 0] <- 1
-targetsMat[targetsMat == 20] <- 0.5
-targetsMat[targetsMat == 10] <- 0
-targetsMat[targetsMat == 30] <- 2
-targetsMat <- targetsMat[rowSums(targetsMat[,5:ncol(targetsMat)])>0,]
+# targetsMat[targetsMat == 2] <- 20
+# targetsMat[targetsMat == 1] <- 10
+# targetsMat[targetsMat == 3] <- 30
+# targetsMat[targetsMat == 0] <- 1
+# targetsMat[targetsMat == 20] <- 0.5
+# targetsMat[targetsMat == 10] <- 0
+# targetsMat[targetsMat == 30] <- 2
 #hclust heatmap
-plotTitle <- paste('../results/plots/targetsMatrix_ALL.png',sep='')
+plotTitle <- paste('../results/plots/targetsMatrix_changing.png',sep='')
 png(plotTitle,width = 5000, height = 13000)
-rownames(targetsMat) <- targetsMat$shortNames
-pheatmap(targetsMat[,5:ncol(targetsMat)],cluster_cols = T,cluster_rows = T, show_rownames = TRUE,scale='none',fontsize = 28)
+rownames(reduced_matrix) <- reduced_matrix$shortNames
+pheatmap(reduced_matrix[,5:ncol(reduced_matrix)],cluster_cols = T,cluster_rows = T, show_rownames = TRUE,scale='none',fontsize = 28)
 dev.off()
 
 #Dimensionality reduction
 newDF <- targetsMat[,5:ncol(targetsMat)]
+genes <- targetsMat$shortNames
+idxs  <- which(rowSums(targetsMat[,5:ncol(targetsMat)])!=cases)
 #newDF <- newDF[rowSums(newDF)>0,]
+newDF <- newDF[idxs,]
+extra <- colnames(newDF)
+genes <- genes[idxs]
+rownames(newDF) <- genes
 newDF <- as.data.frame(t(newDF))
-newDF$extra <- rownames(newDF)
+newDF$extra <- extra
 newDF<- newDF %>% separate(extra, c("chemical", "family"), "_fam_")
-#newDF$family <- factor(newDF$family)
 #Remove duplicate vectors
-idxs <- which(!duplicated(newDF[,1:(ncol(newDF)-3)]))
+idxs       <- which(!duplicated(newDF[,1:(ncol(newDF)-3)]))
+duplicates <- which(duplicated(newDF[,1:(ncol(newDF)-3)]))
+duplicates <- newDF[duplicates,]
 newDF <- newDF[idxs,]
 #Add product family info
 newDF <- newDF[order(newDF$family),]
@@ -127,30 +124,48 @@ saveWidget(p, "../results/plots/tsne_ALL.html", selfcontained = F, libdir = "lib
 
 filename  <- paste('../results/production_targets/targets_summary.txt',sep='')
 targetsDF <- read.csv(filename,sep='\t',stringsAsFactors = FALSE)
+medianTargets <- c()
 for (j in 1:length(families)){
   chemClass <- families[j] 
-  famCode   <- codes[j]
+  famCode   <- 'ALL'#codes[j]
   targets_summary <- targetsDF
-  if (nchar(chemClass)>1){
-    targets_summary <- targets_summary[targets_summary$chemClass==chemClass,]
-  }
+  #if (nchar(chemClass)>1){
+  #  targets_summary <- targets_summary[targets_summary$chemClass==chemClass,]
+  #}
   nCompounds <- nrow(targets_summary)
-  if (nCompounds>=4){
+  if (nCompounds>=1){
+    
     vector1 <- c(targets_summary$cand_1_OE,targets_summary$cand_2_OE,targets_summary$cand_3_OE)
-    vector2 <- c(rep('step1',nCompounds),rep('step2',nCompounds),rep('step3',nCompounds))
+    vector2 <- c(rep('Lvl1',nCompounds),rep('Lvl2',nCompounds),rep('Lvl3',nCompounds))
     vector3 <- c(rep('OE',length(vector1)))
-    df <- data.frame(vector1,vector2,vector3)
+    df      <- data.frame(vector1,vector2,vector3)
+    values  <- cbind(mean(targets_summary$cand_1_OE),mean(targets_summary$cand_2_OE),mean(targets_summary$cand_3_OE))
+    values  <- round(values,2)
+    str     <- gsub('_','',famCode)
+    type    <- 'OE'
+    OErow   <- data.frame(str,values,type)
     
     vector1 <- c(targets_summary$cand_1_dR,targets_summary$cand_2_dR,targets_summary$cand_3_dR)
-    vector2 <- c(rep('step1',nCompounds),rep('step2',nCompounds),rep('step3',nCompounds))
+    vector2 <- c(rep('Lvl1',nCompounds),rep('Lvl2',nCompounds),rep('Lvl3',nCompounds))
     vector3 <- c(rep('KD',length(vector1)))
-    df2 <- data.frame(vector1,vector2,vector3)
+    df2     <- data.frame(vector1,vector2,vector3)
+    values  <- cbind(mean(targets_summary$cand_1_dR),mean(targets_summary$cand_2_dR),mean(targets_summary$cand_3_dR))
+    values  <- round(values,2)
+    str     <- gsub('_','',famCode)
+    type <- 'KD'
+    KDrow   <- data.frame(str,values,type)
     
     vector1 <- c(targets_summary$cand_1_del,targets_summary$cand_2_del,targets_summary$cand_3_del)
-    vector2 <- c(rep('step1',nCompounds),rep('step2',nCompounds),rep('step3',nCompounds))
+    vector2 <- c(rep('Lvl1',nCompounds),rep('Lvl2',nCompounds),rep('Lvl3',nCompounds))
     vector3 <- c(rep('KO',length(vector1)))
-    df3 <- data.frame(vector1,vector2,vector3)
-    df <- rbind(df,df2,df3)
+    df3     <- data.frame(vector1,vector2,vector3)
+    df      <- rbind(df,df2,df3)
+    values  <- cbind(mean(targets_summary$cand_1_del),mean(targets_summary$cand_2_del),mean(targets_summary$cand_3_del))
+    values  <- round(values,2)
+    str     <- gsub('_','',famCode)
+    type <- 'KO'
+    KOrow   <- data.frame(str,values,type)
+    
     colors <- cividis(11)
     #Generate box plots with all targets (KOs, KDs and OEs)
     p <-ggplot(df, aes(x=vector2, y=vector1, fill=vector3)) + geom_boxplot()
@@ -161,5 +176,26 @@ for (j in 1:length(families)){
     png(plotTitle,width = 600, height = 600)
     plot(p)
     dev.off()
+    medianTargets <- rbind(medianTargets,OErow,KDrow,KOrow)
   } 
 }
+colnames(medianTargets) <- c('family','Lvl1','Lvl2','LVl3','type')
+
+actions <- c('OE','KD','KO')
+
+for (action in actions){
+temp <- medianTargets[which(medianTargets$type==action),1:(ncol(medianTargets)-1)]
+temp<-melt(temp,id.vars="family")
+
+p <- ggplot(temp,aes(x=family,y=value,fill=factor(variable)))+
+  geom_bar(stat="identity",position="dodge")+theme_bw(base_size = 2*12) +
+  scale_fill_manual(name='',values = c(colors[10],colors[6],colors[3]))+
+  xlab("Family")+ylab("Mean number of targets per product")
+plotTitle <- paste('../results/plots/meanTargets_AllFams',action,'.pdf',sep='')
+pdf(plotTitle,width = 8.5, height = 7)
+plot(p)
+dev.off()
+}
+write.table(medianTargets,'../results/processed_results/average_targets_number.txt',quote=FALSE,sep='\t',row.names = FALSE)
+
+
